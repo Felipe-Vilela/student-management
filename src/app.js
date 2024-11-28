cde029dc93aaabfdf391584705bf3459e24232e9
Felipe Vilela <felipe.vilela01@outlook.com>
2024-11-27 23:14:18 -0300

    feat: create data mock and get alunos

--- src/app.js
+++ src/app.js
@@ -1,8 +1,42 @@
 import express from "express";
+
 const app = express();
+app.use(express.json());
+
+const alunos = [
+    {
+        id: 1,
+        nome: "Caua Olivio",
+        ra: "SC001",
+        nota1: 9.0,
+        nota2: 10.0
+
+    },
+    {
+        id: 2,
+        nome: "Felipe Vilela",
+        ra: "SC002",
+        nota1: 9.5,
+        nota2: 9.0
+
+    },
+    {
+        id: 3,
+        nome: "Asdrubal Zoroastro",
+        ra: "SC003",
+        nota1: 10.0,
+        nota2: 10.0
+
+    }
+]
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello World!");
+});
+
+
+app.get("/alunos", (req, res) => {
+    res.status(200).json(alunos);
 });
 
 export default app;
